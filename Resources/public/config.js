83736fdfbf1df574268b33d3d87c21ddba0d6bf4
Bhaktaraj Bhatta <bhattabhakta@yarshastudio.com>
2015-07-14 15:55:28 +0545

    nepali fonts added

--- Resources/public/config.js
+++ Resources/public/config.js
@@ -7,4 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    config.contentsCss = '../../../bundles/ivoryckeditor/fonts.css';
+
+    config.font_names = 'Nepali/Nepali; Agra/Agra; Arjun/Arjun; Dev/Dev; Devnew/Devnew; Himalaya/Himalaya; Himali/Himali; Kanchan/Kanchan; Mangal/Mangal; Preeti/Preeti; Ritu/Ritu; Sagar/Sagar;' + config.font_names;
 };
